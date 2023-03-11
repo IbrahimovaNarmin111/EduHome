f92abbcde391e74f473df9a38c2390e8b39e52f5
IbrahimovaNarmin111 <Narmish15@gmail.com>
2023-03-11 15:34:20 +0400

    Init

---
+++ EduHome/EduHome/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using EduHome.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduHome.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ EduHome/EduHome/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using EduHome.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _db;
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+        
+
+        public IActionResult Index()
+        {
+            
+           
+           
+            
+            HomeVM homeVM = new HomeVM
+            {
+                Sliders = _db.Sliders.ToList(),
+                Services = _db.Services.ToList(),
+                Courses= _db.Courses.ToList(),
+                Feedbacks= _db.Feedbacks.ToList(),
+                About = _db.Abouts.FirstOrDefault(),
+                Blogs=_db.Blogs.ToList()
+            };
+            
+            return View(homeVM);
+        }
+
+        
+
+       
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
