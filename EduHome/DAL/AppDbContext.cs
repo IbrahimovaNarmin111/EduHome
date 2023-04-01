26911f65a52429386cb1eb5b11c6ae6bdf5deacb
IbrahimovaNarmin111 <Narmish15@gmail.com>
2023-04-01 22:08:03 +0400

    Delete yazdiq

---
+++ EduHome/DAL/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EduHome.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace EduHome.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Service>Services { get; set; }
+        public DbSet<Course>Courses { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Bio> Bios { get; set; }
+
+        internal Task SaveChangeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EduHome/ViewComponents/CoursesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EduHome.ViewComponents
+{
+    public class CoursesViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _db;
+        public CoursesViewComponent(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Course> courses = await _db.Courses.ToListAsync();
+            return View(courses);
+        }
+    }
+}
