6f7a90eeeb7a751a311bb016a072167fce222216
IbrahimovaNarmin111 <Narmish15@gmail.com>
2023-04-27 00:35:11 +0400

    Identity

--- EduHome/DAL/AppDbContext.cs
+++ EduHome/DAL/AppDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using EduHome.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
 
 namespace EduHome.DAL
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext:IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
         {
